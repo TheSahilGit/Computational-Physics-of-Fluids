2cdaff47524293c76977a1be411561b31a86d707
Sahil Islam <thesahil.islam@gmail.com>
2023-01-29 12:24:58 +0530

    Added the rule of 72. And some other .

--- logxx.cpp
+++ logxx.cpp
@@ -14,21 +14,63 @@
 
 using namespace std;
 
-//Declaration of function. 
 
-float f(float x);
-float logox(float x, int n);
+// ************************
+// Functions 
+// ************************
 
+
+//Function for the Trapezoidal rule
+
+float f(float x){
+    float fx;
+    fx = 1/(1+x);
+   // fx = x*x ;
+    return fx; 
+}
+
+
+
+// Taylor expansion of log(1+x)
+float logox(float x, int n){
+  float ss;
+  string invalid;
+  int i;
+
+  ss = 0;
+
+
+  for(i=1;i<=n;i++){
+    //ss = ss + pow((-1),i+1) * pow(x,i) / i ;
+    ss +=  pow((-1),i+1) * pow(x,i) / i ;
+    //ss = ss +  ((-1)^(i+1)) * (x^i) / i;
+  }
+
+  return ss;
+
+}
+
+// *********************************************************
 // Main Code
-
+// *********************************************************
+//
 int main(){
     int i,j;
-    float ff, h, a, b, n;
+    float ff, h, a, b,r, n;
     float midsum;
     float tlog;
+    float td, logtwo;
+    float fss;
 
+
+    r = 8;
+
+
+// Trapezoidal happening. 
+// **********************************************************
+    
     a = 0;
-    b = 0.5;
+    b = r/100.0;
     n = 100;
 
     h = (b-a)/n;
@@ -42,47 +84,34 @@ int main(){
 
     ff = 0.5 * h * (f(a) + f(b) + 2 * midsum);
 
-    tlog = logox(b,n);
+// *********************************************************
+
+    if(b>1){
+      cout << "Taylor series doesn't converge for x>1 \n";
+    }
+    else{
+      tlog = logox(b,n);
+      cout << "Taylor approx: " << tlog << "\n" ;
+    }
+
+// *********************************************************
+// Rule of 72 
+// *********************************************************
+// Here we assume b% interest. That means time taken would be
+// T = b*log(2)/log(1+b/100); This should reach 72. 
+// *******************************************************
+    
+    td = r*log(2)/ff;
+    
+
 
 //Printing
     
     cout << "Trapezoid approx: " << ff << "\n";
-    cout << "Taylor approx: " << tlog << "\n" ;
+    cout << "log(1+" << b << ") from math.h : " << log(1+b) << "\n"; 
+    cout << "Time taken for the amount to be double: " << td << "\n";
 
     return 0;
-}
-
-// ************************
-// Functions 
-// ************************
-
-
-//Function for the Trapezoidal rule
-float f(float x){
-    float fx;
-    fx = 1/(1+x);
-   // fx = x*x ;
-    return fx; 
-
-}
-
-
-
-// Taylor expansion of log(1+x)
-float logox(float x, int n){
-  float ss;
-  int i;
-
-  ss = 0;
-
-  for(i=1;i<=n;i++){
-    ss = ss + pow(-1,i+1)*pow(x,i)/i;
-  }
-
-  return ss;
-
-
-
 }
 
 
